7fad248b641f5436fca46d546eefa9ce555b5442
Alima <aliali.ali1378@yahoo.com>
2023-04-24 01:09:13 +0430

    create redis connection

--- database/Redis.go
+++ database/Redis.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+func CreateConnection() func() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("redis_host"),
+		Password: "",
+		DB:       0,
+	})
+	return func() *redis.Client {
+		return client
+	}
+}
+
+func GetRedisClient() *redis.Client {
+	client := CreateConnection()
+	return client()
+}
