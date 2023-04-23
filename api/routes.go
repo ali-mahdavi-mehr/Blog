23e963c5159fa77e853e87a838146cb98185f4b2
Alima <aliali.ali1378@yahoo.com>
2023-04-24 02:42:24 +0430

    add authenticate route

--- api/routes.go
+++ api/routes.go
@@ -3,7 +3,10 @@ package api
 import "github.com/labstack/echo"
 
 func RegisterAPIs(router *echo.Group) {
+	//Auth
+	router.POST("/auth/login/", Login)
 
+	//Posts
 	router.GET("/post/", GetAllPost)
 	router.GET("/post/:id/", GetOnePost)
 	router.POST("/post/", CreatePost)
