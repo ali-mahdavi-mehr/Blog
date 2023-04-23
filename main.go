1a35ac96bf5d7a044177167707235290b206822a
Alima <aliali.ali1378@yahoo.com>
2023-04-24 04:27:28 +0430

    Auto migration for models

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/alima12/Blog-Go/api"
+	"github.com/alima12/Blog-Go/models"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	_ = godotenv.Load(".env")
+	models.MigrateModels()
 	e := echo.New()
 	e.GET("/", api.Home)
 	api.RegisterAPIs(e.Group("/api"))
