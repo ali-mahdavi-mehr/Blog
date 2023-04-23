cd1f220a5bdb1f7ac0e0cb032d07840506bd69e5
Alima <aliali.ali1378@yahoo.com>
2023-04-24 01:25:06 +0430

    postgres connection

--- database/Postgress.go
+++ database/Postgress.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+func createPostgresConnection() func() *gorm.DB {
+	dataBaseConnection := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tehran",
+		os.Getenv("postgres_host"),
+		os.Getenv("postgres_user"),
+		os.Getenv("postgres_password"),
+		os.Getenv("postgres_db_name"),
+		os.Getenv("postgres_port"))
+	db, err := gorm.Open(postgres.Open(dataBaseConnection), &gorm.Config{})
+	if err != nil {
+		panic("error in connection")
+	}
+	return func() *gorm.DB {
+		return db
+	}
+}
+
+func GetDB() *gorm.DB {
+	client := createPostgresConnection()
+	return client()
+}
