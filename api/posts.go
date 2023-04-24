26446a52eaf566819dd3ede2609445deb6067e4e
Alima <aliali.ali1378@yahoo.com>
2023-04-24 04:58:36 +0430

    get all post and get post detail get data from database

--- api/posts.go
+++ api/posts.go
@@ -1,17 +1,33 @@
 package api
 
 import (
+	"github.com/alima12/Blog-Go/database"
+	"github.com/alima12/Blog-Go/models"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
 func GetAllPost(c echo.Context) error {
-	return c.String(http.StatusOK, "ok")
+	db := database.GetDB()
+
+	var posts []models.Post
+	err := db.Model(&models.Post{}).Find(&posts).Error
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, posts)
 
 }
 
 func GetOnePost(c echo.Context) error {
-	return c.String(http.StatusOK, "one post")
+	db := database.GetDB()
+	id := c.Param("id")
+	var post models.Post
+	result := db.First(&post, id)
+	if result.Error != nil {
+		return c.String(http.StatusNotFound, "post not found")
+	}
+	return c.JSON(http.StatusOK, post)
 }
 
 func CreatePost(c echo.Context) error {
