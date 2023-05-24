0df808210e3c7e46cb3c63097d7983f220b7c750
Ali <aliali.ali1378@yahoo.com>
2023-05-24 19:07:47 +0330

    Cleaning routes

--- api/routes.go
+++ api/routes.go
@@ -7,20 +7,20 @@ import (
 
 func RegisterAPIs(router *echo.Group) {
 	//Auth
-	authGroup := router.Group("/auth/")
-	authGroup.POST("login/", Login)
-	authGroup.POST("refresh-token/", RefreshToken)
-	authGroup.POST("sign-up/", SignUp)
-	authGroup.GET("logout/", Logout, middlewares.LoginRequired)
-	authGroup.POST("change-password/", ChangePassword, middlewares.LoginRequired)
-	authGroup.POST("delete-account/", DeleteAccount, middlewares.LoginRequired)
+	authGroup := router.Group("/auth")
+	authGroup.POST("/login/", Login)
+	authGroup.POST("/refresh-token/", RefreshToken)
+	authGroup.POST("/sign-up/", SignUp)
+	authGroup.GET("/logout/", Logout, middlewares.LoginRequired)
+	authGroup.POST("/change-password/", ChangePassword, middlewares.LoginRequired)
+	authGroup.POST("/delete-account/", DeleteAccount, middlewares.LoginRequired)
 
 	//Posts
-	postRouter := router.Group("/post/")
-	postRouter.GET("", GetAllPost)
-	postRouter.GET(":slug/", GetOnePost, middlewares.LoginRequired)
-	postRouter.POST("", CreatePost, middlewares.LoginRequired)
-	postRouter.PUT(":slug/", UpdatePost, middlewares.LoginRequired)
-	postRouter.DELETE(":slug/", DeletePost, middlewares.LoginRequired)
+	postRouter := router.Group("/post")
+	postRouter.GET("/", GetAllPost)
+	postRouter.GET("/:slug/", GetOnePost, middlewares.LoginRequired)
+	postRouter.POST("/", CreatePost, middlewares.LoginRequired)
+	postRouter.PUT("/:slug/", UpdatePost, middlewares.LoginRequired)
+	postRouter.DELETE("/:slug/", DeletePost, middlewares.LoginRequired)
 
 }
